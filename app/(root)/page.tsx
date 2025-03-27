581dada12d65d654fe99f99caf4ecfe5bef32aec
mbanixox <46408125+mbanixox@users.noreply.github.com>
2025-03-27 14:01:31 +0300

    Add Search Bar component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,9 @@
-export default function Home() {
+import SearchBar from "../../components/SearchBar";
+
+export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
+
+  const query = (await searchParams).query;
+  
   return (
     <>
       <section className="home_container">
@@ -7,11 +12,13 @@ export default function Home() {
             Trade-in your games. Sell your games. Buy games.
           </h1>
 
-          <p className="text-xl text-[#010D3E] tracking-tight mt-6">
-            Trade your games for other games from a community of gamers. Sell
+          <p className="text-xl text-[#010D3E] tracking-tight mt-6 mb-10">
+            Trade your games with a community of gamers. Sell
             your games to other gamers. Buy games from other gamers. All in one
             place.
           </p>
+
+          <SearchBar query={query} />
         </div>
       </section>
     </>
---
+++ components/SearchBar.tsx
@@ -0,0 +1,24 @@
+import { Search } from "lucide-react";
+import Form from "next/form";
+
+const SearchBar = ({ query }: { query?: string }) => {
+
+  return (
+    <Form action="/" scroll={false} className="search-bar">
+      <input
+        name="query"
+        defaultValue={query}
+        className="search-input"
+        placeholder="Search for games..."
+      />
+
+      <div className="flex gap-2">
+        <button type="submit" className="search-btn">
+            <Search className="size-5"/>
+        </button>
+      </div>
+    </Form>
+  );
+};
+
+export default SearchBar;
